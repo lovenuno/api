ddb208a7ef7912399df167e1d02ce5c6bc50365b
love NUNO <nuno08@gmail.com>
2023-01-28 10:42:34 +0900

    tttt

--- src/main/java/com/springboot/api/ApiApplication.java
+++ src/main/java/com/springboot/api/ApiApplication.java
@@ -3,6 +3,7 @@ package com.springboot.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Git test
 @SpringBootApplication
 public class ApiApplication {
 
